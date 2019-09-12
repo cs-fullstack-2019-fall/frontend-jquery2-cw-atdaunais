e1fb2e578efb34e637dc6550fe5a332ddc5cab30
atdaunais <atdaunais@gmail.com>
2019-09-12 15:47:53 -0500

    completed and tested assignment and challenge

--- js.js
+++ js.js
@@ -3,10 +3,27 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 */
 
 
-/* Give everything with the class 'hello' a white background when you click on one of them*/
+/* Give everything with the class 'hello' a background color of your choosing when you click on one of them*/
+$(".hello").click(function (e) {
+    $(e.target).css("backgroundColor","blue")
+});
+
+/* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it
+change it back */
+$("h1").mouseenter(function(e){
+    $(e.target).css("width","20px");
+    $(e.target).css("height","20px")
+});
+
+$("h1").mouseleave(function(e){
+    $(e.target).css("width","");
+    $(e.target).css("height","")
+});
 
 
-/* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it change it back */
 
 // CHALLENGE
 /* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
+$(".hello:first").click(function (e) {
+    $(e.target).append(".")
+});
